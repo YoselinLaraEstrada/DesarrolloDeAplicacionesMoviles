f28721ff4bc141369f846c7e4d72d0ff2157bcaa
Yoselin Lara Estrada <56008423+YoselinLaraEstrada@users.noreply.github.com>
2020-04-05 20:18:44 -0500

    PracticaGaleria
    Galeria de imagenes

---
+++ Imagenes/app/src/main/java/tesoem/edu/mx/imagenes/aleatorio.java
@@ -0,0 +1,19 @@
+package tesoem.edu.mx.imagenes;
+
+public class aleatorio {
+    int Imagen;
+    String aleatorio;
+    public aleatorio(int Imagen,String aleatorio){
+        this.Imagen = Imagen;
+        this.aleatorio = aleatorio;
+    }
+
+    public int getImagen() {
+        return Imagen;
+    }
+
+    public String getAleatorio() {
+        return aleatorio;
+    }
+}
+
